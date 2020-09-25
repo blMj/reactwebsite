bf3727a236df41b7ac521b4706da437b7cece57c
Surath Jhakal <46605283+surathjhakal@users.noreply.github.com>
2020-09-25 17:30:10 +0530

    Updates

---
+++ precache-manifest.bd770f3b6b23ddbeefafdf39bc9270f7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58fbc885d7a44a50ff18f8409a0d0561",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "6e598e0451ddd040279f",
+    "url": "/reactwebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "1905da6e096ec90cd8cd",
+    "url": "/reactwebsite/static/css/main.ad44af00.chunk.css"
+  },
+  {
+    "revision": "6e598e0451ddd040279f",
+    "url": "/reactwebsite/static/js/2.2ed54442.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactwebsite/static/js/2.2ed54442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1905da6e096ec90cd8cd",
+    "url": "/reactwebsite/static/js/main.90189683.chunk.js"
+  },
+  {
+    "revision": "2d573c69793dc347f711",
+    "url": "/reactwebsite/static/js/runtime-main.6337ff56.js"
+  },
+  {
+    "revision": "42225f5baebef0bbefd3de15c61d42a8",
+    "url": "/reactwebsite/static/media/android.42225f5b.jpg"
+  },
+  {
+    "revision": "3b2992312eb4b1c63a63c351dfaf3355",
+    "url": "/reactwebsite/static/media/app.3b299231.png"
+  },
+  {
+    "revision": "cd810b8c501524d070267a6420687fd6",
+    "url": "/reactwebsite/static/media/blockchain.cd810b8c.png"
+  },
+  {
+    "revision": "2472d9b186e109315efa032c5df903d0",
+    "url": "/reactwebsite/static/media/digital.2472d9b1.webp"
+  },
+  {
+    "revision": "b68fdc0293b311dfb8ee8fdb55e15e12",
+    "url": "/reactwebsite/static/media/rocket.b68fdc02.jpg"
+  },
+  {
+    "revision": "1b7d4e6458497f338eea4e10d9a2fdb2",
+    "url": "/reactwebsite/static/media/software.1b7d4e64.png"
+  },
+  {
+    "revision": "103b855b69b5f2db160326e4caf25a12",
+    "url": "/reactwebsite/static/media/tech.103b855b.jpg"
+  },
+  {
+    "revision": "57e269d772efb557362226367e9e2a63",
+    "url": "/reactwebsite/static/media/web.57e269d7.jpeg"
+  }
+]);
